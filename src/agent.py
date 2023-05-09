4eeb10eaf23662d2b40d7ed0067584d4a212c849
palebluepaul <paul.scott@paleblueconsulting.co.uk>
2023-05-09 22:21:05 +0200

    Temp and Token Config

--- src/agent.py
+++ src/agent.py
@@ -7,6 +7,9 @@ class Agent:
         self.mode = mode
         self.api_key = api_key
         self.logger = logger
+        self.temperature = agent_config['temperature'] if 'temperature' in agent_config else 0.7
+        self.max_tokens = agent_config['max_tokens'] if 'max_tokens' in agent_config else 500
+
 
     def call_agent(self, content):
         # Generate the message to the chat model
@@ -25,7 +28,9 @@ class Agent:
         openai.api_key = self.api_key
         response = openai.ChatCompletion.create(
           model="gpt-4",
-          messages=[message, message2]
+          messages=[message, message2],
+          max_tokens=self.max_tokens,
+          temperature=self.temperature
         )
 
         output = response['choices'][0]['message']['content']
