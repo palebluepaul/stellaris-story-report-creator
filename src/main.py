9ad576d64772b2d9a607ed12c9a1d0c1cfe6c174
palebluepaul <paul.scott@paleblueconsulting.co.uk>
2023-05-09 20:10:35 +0200

    One agent working

--- src/main.py
+++ src/main.py
@@ -1,15 +1,40 @@
 import os
 import sys
 import json
+import openai
+from dotenv import load_dotenv
 
 def main(mode, input_file):
+    # Load API key from environment
+    load_dotenv()
+    openai.api_key = os.getenv('OPENAI_API_KEY')
+
+    # Read the agent configuration
+    with open('config/agent_config.json', 'r') as f:
+        agent_config = json.load(f)['novelist']
+
     # Read the input file
     with open(input_file, 'r') as f:
         request = f.read()
 
-    # For now, we're just echoing the input to the output
-    # Later, we'll replace this with a call to the agent
-    output = request
+    # Generate the message to the chat model
+    message = {
+        'role': 'system',
+        'content': agent_config['system']
+    }
+    message2 = {
+        'role': 'user',
+        'content': agent_config[mode] + request
+    }
+
+    # Call OpenAI's GPT-4 chat completion API
+    response = openai.ChatCompletion.create(
+      model="gpt-4",
+      messages=[message, message2]
+    )
+
+    # Use the response as the output
+    output = response['choices'][0]['message']['content']
 
     # Write the output to a file in the output directory
     output_file = os.path.join('output', f'{os.path.splitext(os.path.basename(input_file))[0]}_output.txt')
