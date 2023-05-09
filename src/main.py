a71f3ac2c29c01c1bdf6500a56c1fb688b8274c9
palebluepaul <paul.scott@paleblueconsulting.co.uk>
2023-05-09 20:23:35 +0200

    Editor implemented

--- src/main.py
+++ src/main.py
@@ -4,27 +4,15 @@ import json
 import openai
 from dotenv import load_dotenv
 
-def main(mode, input_file):
-    # Load API key from environment
-    load_dotenv()
-    openai.api_key = os.getenv('OPENAI_API_KEY')
-
-    # Read the agent configuration
-    with open('config/agent_config.json', 'r') as f:
-        agent_config = json.load(f)['novelist']
-
-    # Read the input file
-    with open(input_file, 'r') as f:
-        request = f.read()
-
+def call_agent(agent, mode, content):
     # Generate the message to the chat model
     message = {
         'role': 'system',
-        'content': agent_config['system']
+        'content': agent['system']
     }
     message2 = {
         'role': 'user',
-        'content': agent_config[mode] + request
+        'content': agent[mode] + content
     }
 
     # Call OpenAI's GPT-4 chat completion API
@@ -33,13 +21,35 @@ def main(mode, input_file):
       messages=[message, message2]
     )
 
-    # Use the response as the output
-    output = response['choices'][0]['message']['content']
+    # Return the response
+    return response['choices'][0]['message']['content']
 
-    # Write the output to a file in the output directory
+def main(mode, input_file):
+    # Load API key from environment
+    load_dotenv()
+    openai.api_key = os.getenv('OPENAI_API_KEY')
+
+    # Read the agent configuration
+    with open('config/agent_config.json', 'r') as f:
+        agent_config = json.load(f)
+
+    # Read the input file
+    with open(input_file, 'r') as f:
+        request = f.read()
+
+    # Call the novelist agent
+    novelist_output = call_agent(agent_config['novelist'], mode, request)
+
+    # Call the editor agent
+    editor_feedback = call_agent(agent_config['editor'], mode, novelist_output)
+
+    # Call the novelist agent again with the editor's feedback
+    revised_output = call_agent(agent_config['novelist'], mode, editor_feedback)
+
+    # Write the revised output to a file in the output directory
     output_file = os.path.join('output', f'{os.path.splitext(os.path.basename(input_file))[0]}_output.txt')
     with open(output_file, 'w') as f:
-        f.write(output)
+        f.write(revised_output)
 
     print(f'Successfully wrote output to {output_file}')
 
